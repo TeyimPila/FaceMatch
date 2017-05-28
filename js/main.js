6089113f99b376ebef99879b87957d3a9f996c54
Teyim Pila <pila@Teyims-MacBook-Pro.local>
2017-05-28 22:42:38 +0100

    complete game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var numberOfFaces = 5;
 var leftSide = document.getElementById('leftSide');
+var rightSide = document.getElementById('rightSide');
+var theBody = document.getElementsByTagName('body')[0];
+var resetButton = document.getElementById('reset');
+var score = 0;
+var successClicks = 0;
 
 function generateFaces() {
+    document.getElementById('score').innerHTML = score;
 
     for (var i = 1; i <= numberOfFaces; i++) {
         var img = document.createElement('img');
@@ -10,6 +16,53 @@ function generateFaces() {
         img.style.left = Math.floor(Math.random() * 400 + 1) + 'px';
         leftSide.appendChild(img);
     }
+
+    leftSideImages = leftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+
+    while (leftSideImages.hasChildNodes()){
+        rightSide.appendChild(leftSideImages.firstChild);
+    }
+
+    //adding click event to the lone face
+    leftSide.lastChild.onclick = function nextLevel(event) {
+        event.stopPropagation();
+        numberOfFaces += 5;
+        score += 5;
+        successClicks += 1;
+
+        generateFaces();
+    };
+
+    document.onclick = function(evt) {
+        var evt = window.event || evt; // window.event for IE
+        if (!evt.target) evt.target=evt.srcElement; // extend target property for IE
+
+        //reset Game if reset button is pressed
+        if(evt.target.id === resetButton.id){
+            score = 0;
+            successClicks = 0;
+            numberOfFaces = 5;
+            while (leftSide.hasChildNodes()){
+                leftSide.removeChild(leftSide.firstChild);
+            }
+
+            while (rightSide.hasChildNodes()){
+                rightSide.removeChild(rightSide.firstChild);
+            }
+            generateFaces();
+        }
+    };
+
+    // //adding click event for the body
+    theBody.onclick = function gameOver() {
+
+        alert("Game Over! \nClicks: " + successClicks + " \nScore: " + score);
+
+        theBody.onclick = null;
+
+        theLeftSide.lastChild.onclick = null;
+    };
 
 }
 
